765b4e1567f45535bd02f72995c4c9d0f6bd7f01
Kanmani Kamalanathan <73705050+kanmani-kamalanathan@users.noreply.github.com>
2022-11-21 12:17:55 +0530

    Update app.py

--- Final Deliverables/Source Code/app.py
+++ Final Deliverables/Source Code/app.py
@@ -1,9 +1,3 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Tue Nov  8 15:02:49 2022
-@author: Sneha
-"""
-
 from flask import Flask, render_template, request
 # Flask-It is our framework which we are going to use to run/serve our application.
 #request-for accessing file which was uploaded by the user on our application.
